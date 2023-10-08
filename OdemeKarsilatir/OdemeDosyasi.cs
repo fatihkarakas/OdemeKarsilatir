95659c9fd6a3ade5db93512e9bdfe5fd850c7012
Fatih KARAKAŞ <fkarakasss@gmail.com>
2023-10-08 21:32:33 +0300

    Fatih KARAKAŞ

--- OdemeKarsilatir/OdemeDosyasi.cs
+++ OdemeKarsilatir/OdemeDosyasi.cs
@@ -2,13 +2,10 @@
 using iTextSharp.text.pdf.parser;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClosedXML.Excel;
 
